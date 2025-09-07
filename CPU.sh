c9ba3980ea3d614fdc7a72db7091ecb233dd5f6e
Shravan Marati <coolshra1@gmail.com>
2025-09-07 19:19:29 +0530

    Update CPU.sh

--- CPU.sh
+++ CPU.sh
@@ -1,7 +1,7 @@
 !# /bin/bash
 
 ##running while loop wihtout break 
-##This script will make make CPU usage 100%
+##This script will make CPU usage 100%
 ##Do not execute this in prod env
 ##For better undertanding watch my yt chanenl vid: "https://youtu.be/Yj0IGIyBD64?si=PKF4Kxypmz3OpD35"
 
