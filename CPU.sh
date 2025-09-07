36784c20ec618079895156301635994d5fc21af9
Shravan Marati <coolshra1@gmail.com>
2025-09-07 18:25:58 +0530

    linux-stress-test scripts
    simple shell scripts to test cpu and ram..

---
+++ CPU.sh
@@ -0,0 +1,12 @@
+!# /bin/bash
+
+##running while loop wihtout break 
+##This script will make make CPU usage 100%
+##Do not execute this in prod env
+##For better undertanding watch my yt chanenl vid: "https://youtu.be/Yj0IGIyBD64?si=PKF4Kxypmz3OpD35"
+
+While true
+  do 
+    echo "Increase CPU usage" >> /dev/null
+  done
+  
---
+++ memory.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+##This script will increase memory usage to 100%.
+##Do not execute this in prod env
+##For better undertanding watch my yt chanenl vid: "https://youtu.be/uhd0CUDSlVo?si=zm0omtQtb5cU5Zu7"
+## Designed for 2 GB RAM—adjust the value by adding more zeros to scale for higher memory.
+
+while true
+  do
+    echo {1..1000000}
+  done 
